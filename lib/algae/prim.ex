c204c1693cbcf552187dcd3e6cb91954aab43773
Attila Gulyas <toraritte@gmail.com>
2019-03-01 12:26:49 -0800

    Add boolean and list to Prim

--- lib/algae/prim.ex
+++ lib/algae/prim.ex
@@ -4,4 +4,10 @@ defmodule Algae.Prim do
 
   def string(str) when is_binary(str), do: str
   def string(_), do: raise(ArgumentError, "not string")
+
+  def boolean(bool) when is_boolean(bool), do: bool
+  def boolean(_), do: raise(ArgumentError, "not boolean")
+
+  def list(list) when is_list(list), do: list
+  def list(_), do: raise(ArgumentError, "not list")
 end
