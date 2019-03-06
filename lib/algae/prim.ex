2c3f992c969aba2e52fee9c440d1a698c90c6e0b
Attila Gulyas <toraritte@gmail.com>
2019-03-05 16:23:43 -0800

    Add binary base type to Prim

--- lib/algae/prim.ex
+++ lib/algae/prim.ex
@@ -10,5 +10,8 @@ defmodule Algae.Prim do
 
   def float(float) when is_float(float), do: float
   def float(_), do: raise(ArgumentError, "not float")
+
+  def binary(bin) when is_binary(bin), do: bin
+  def binary(_), do: raise(ArgumentError, "not binary")
 end
 
